d4104f37afe1260f68b5e4bffe9aaa2308e36775
tjpc3 <hotwire567@gmail.com>
2017-06-04 19:43:03 -0400

    sdgf

--- src/com/tjpc3/perlinnoisemapmaker/generation/BiomeMap.java
+++ src/com/tjpc3/perlinnoisemapmaker/generation/BiomeMap.java
@@ -40,7 +40,7 @@ public class BiomeMap extends Map {
 			pixels[i] = color(elevation.pixels[i], moisture.pixels[i]);
 		}
 		
-		for (int c = 0; c < 1; c++) {
+		for (int c = 0; c < 10; c++) {
 			// Start source selection
 			Vector2D sourceVector = null;
 			double sourceElevation = -1.0;
@@ -49,48 +49,83 @@ public class BiomeMap extends Map {
 				int randY = rand.nextInt(height);
 				
 				sourceElevation = elevation.getPixel(randX, randY);
-				if (sourceElevation >= 0.9) { // The minimum elevation rivers must start at
+				if (sourceElevation >= 0.7) { // The minimum elevation rivers must start at
 					sourceVector = new Vector2D(randX, randY);
 				}
 			}
 			// End source selection
 			
-			double currentLowestElevation = Double.MAX_VALUE;
-			Vector2D currentLowestPos = new Vector2D(sourceVector.getX(), sourceVector.getY());
+			carveRiver(sourceVector, elevation, rand);
+		}
+	}
+	
+	private void carveRiver(Vector2D sourceVector, Noise elevation, Random rand) {
+		double sourceElevation;
+		
+		int count = 0;
+		while (true) {
+			count++;
+			pixels[sourceVector.getX() + sourceVector.getY() * width] = 0x0000FF;
+			sourceElevation = elevation.getPixel(sourceVector.getX(), sourceVector.getY());
 			
-			int count = 0;
-			while (count < 1000) {
-				count++;
-				pixels[sourceVector.getX() + sourceVector.getY() * width] = 0x0000FF;
-				sourceElevation = elevation.getPixel(sourceVector.getX(), sourceVector.getY());
-				
-				for (int y = -1; y < 2; y++) {
-					for (int x = -1; x < 2; x++) {
-						if (x == 0 && y == 0)
-							continue;
-						//System.out.println("X: " + x + " Y: " + y);
-						double testElevation = elevation.pixels[(sourceVector.getX() + x) + (sourceVector.getY() + y) * elevation.getWidth()];
-						
-						if (testElevation == -1.0)
-							continue;
-						
-						if (testElevation < currentLowestElevation) {
-							currentLowestElevation = testElevation;
-							currentLowestPos = new Vector2D(sourceVector.getX() + x, sourceVector.getY() + y);
-						}
+			double currentLowestElevation = Double.MAX_VALUE;
+			Vector2D currentLowestPos = new Vector2D(sourceVector.getX() - 1, sourceVector.getY() - 1);
+			
+			for (int y = -1; y < 2; y++) {
+				for (int x = -1; x < 2; x++) {
+					if (x == 0 && y == 0)
+						continue;
+					//System.out.println("X: " + x + " Y: " + y);
+					double testElevation = elevation.pixels[(sourceVector.getX() + x) + (sourceVector.getY() + y) * elevation.getWidth()];
+					
+					if (testElevation == (double) 2.0)
+						continue;
+					
+					if (count >= 600 && rand.nextInt(1000) == 0)
+						carveRiver(new Vector2D(sourceVector.getX() + x, sourceVector.getY() + y), elevation, rand);
+					
+					if (testElevation < currentLowestElevation) {
+						currentLowestElevation = testElevation;
+						currentLowestPos = new Vector2D(sourceVector.getX() + x, sourceVector.getY() + y);
 					}
 				}
-				
-				if (currentLowestElevation < 0.3)
-					break;
-				
-				if (currentLowestElevation >= sourceElevation) { // If river got into a hole
-					System.out.println("test");
-					elevation.pixels[sourceVector.getX() + sourceVector.getY() * elevation.getWidth()] = -1.0;
-				}
-				
-				sourceVector = currentLowestPos;
 			}
+			
+			if (currentLowestElevation < 0.3)
+				break;
+			
+//			if (currentLowestElevation >= sourceElevation) { // If river got into a hole
+//				System.out.println("test");
+//				elevation.pixels[sourceVector.getX() + sourceVector.getY() * elevation.getWidth()] = 2.0;
+//			}
+			
+			elevation.pixels[sourceVector.getX() + sourceVector.getY() * elevation.getWidth()] = 2.0;
+			
+			int radius = 6;
+			
+//			if (currentLowestPos.getX() != sourceVector.getX()) {
+//				for (int i = 1; i < radius; i++) {
+//					elevation.pixels[(sourceVector.getX() - i) + sourceVector.getY() * elevation.getWidth()] = 2.0;
+//					elevation.pixels[(sourceVector.getX() + i) + sourceVector.getY() * elevation.getWidth()] = 2.0;
+//				}
+//			}
+//			
+//			if (currentLowestPos.getY() != sourceVector.getY()) {
+//				for (int i = 1; i < radius; i++) {
+//					elevation.pixels[sourceVector.getX() + (sourceVector.getY() - i) * elevation.getWidth()] = 2.0;
+//					elevation.pixels[sourceVector.getX() + (sourceVector.getY() + i) * elevation.getWidth()] = 2.0;
+//				}
+//			}
+			
+			for (int i = 1; i < radius; i++) {
+				elevation.pixels[(sourceVector.getX() - i) + (sourceVector.getY() - i) * elevation.getWidth()] = 2.0;
+				elevation.pixels[(sourceVector.getX() + i) + (sourceVector.getY() - i) * elevation.getWidth()] = 2.0;
+				
+				elevation.pixels[(sourceVector.getX() - i) + (sourceVector.getY() + i) * elevation.getWidth()] = 2.0;
+				elevation.pixels[(sourceVector.getX() + i) + (sourceVector.getY() + i) * elevation.getWidth()] = 2.0;
+			}
+			
+			sourceVector = currentLowestPos;
 		}
 	}
 	
